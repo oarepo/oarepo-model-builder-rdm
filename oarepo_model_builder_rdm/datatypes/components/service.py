d537f4bb58129ba7e3f4624a4d097e7cabb7494e
Alzbeta Pokorna <alzbetapokorna@eduroam-tlh247.cesnet.cz>
2025-01-28 13:32:32 +0100

    components dependency

--- oarepo_model_builder_rdm/datatypes/components/service.py
+++ oarepo_model_builder_rdm/datatypes/components/service.py
@@ -1,11 +1,11 @@
 from oarepo_model_builder.datatypes import DataTypeComponent
 from oarepo_model_builder.datatypes.components.model import ServiceModelComponent
 from oarepo_model_builder.datatypes.model import ModelDataType
-from oarepo_model_builder.datatypes.components.model.utils import set_default
+from oarepo_model_builder_files.datatypes.components import ParentRecordComponent
 
 class RDMServiceComponent(DataTypeComponent):
     eligible_datatypes = [ModelDataType]
-    depends_on = [ServiceModelComponent]
+    depends_on = [ServiceModelComponent, ParentRecordComponent]
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
 
