921942ba87115784be8ea67c1b5f82d4d5823cd7
Alzbeta Pokorna <alzbetapokorna@Alzbetas-MacBook-Pro.local>
2025-02-04 07:44:40 +0100

    test script

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 OAREPO_VERSION=${OAREPO_VERSION:-12}
-PYTHON=${PYTHON:-python3.12}
+PYTHON=${PYTHON:-python3}
 export PIP_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
 export UV_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
 
