1eed796d9b7f535498062ef6c6a3ee66083f71e3
Alzbeta Pokorna <alzbetapokorna@Alzbetas-MacBook-Pro.local>
2025-02-12 15:22:07 +0100

    search options

--- oarepo_model_builder_rdm/datatypes/components/search_options.py
+++ oarepo_model_builder_rdm/datatypes/components/search_options.py
@@ -4,13 +4,14 @@ from oarepo_model_builder.datatypes import DataTypeComponent
 
 class RDMSearchOptionsModelComponent(DataTypeComponent):
     eligible_datatypes = [ModelDataType]
-    depends_on = [SearchOptionsModelComponent] #todo: depends on draft search options - when implemented
+    depends_on = [SearchOptionsModelComponent]
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
         if datatype.root.profile == "record":
             module = datatype.definition["module"]["qualified"]
             record_search_prefix = datatype.definition["module"]["prefix"]
             datatype.definition["search-options"]["versions"] = {"class":f"{module}.{record_search_prefix}VersionsSearchOptions","base-classes": ["invenio_rdm_records.services.config.RDMSearchVersionsOptions"]}
-            datatype.definition["search-options"]["base-classes"] = ["invenio_rdm_records.services.config.RDMSearchOptions"]
+            datatype.definition["search-options"]["base-classes"] = ["oarepo_runtime.services.search.I18nRDMSearchOptions"]
         elif datatype.root.profile == "draft":
-            datatype.definition["search-options"]["base-classes"] = ["invenio_rdm_records.services.config.RDMSearchDraftsOptions"]
+            datatype.definition["search-options"]["base-classes"] = ["oarepo_runtime.services.search.I18nRDMDraftsSearchOptions"]
+
