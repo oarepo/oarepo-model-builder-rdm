131dd5df5bbee9c914483871467b38574ccd48b9
Mirek Simek <miroslav.simek@gmail.com>
2025-03-13 14:17:08 +0100

    Fixing service(-config) overwrite

--- oarepo_model_builder_rdm/datatypes/components/service.py
+++ oarepo_model_builder_rdm/datatypes/components/service.py
@@ -3,39 +3,62 @@ from oarepo_model_builder.datatypes.components.model import ServiceModelComponen
 from oarepo_model_builder.datatypes.model import ModelDataType
 from oarepo_model_builder_files.datatypes.components import ParentRecordComponent
 
+PLAIN_RECORD_SERVICE = (
+    "invenio_records_resources.services.RecordService{InvenioRecordService}"
+)
+DRAFT_RECORD_SERVICE = (
+    "invenio_drafts_resources.services.RecordService{InvenioRecordService}"
+)
+RDM_RECORD_SERVICE = "invenio_rdm_records.services.services.RDMRecordService"
+
+PLAIN_SERVICE_CONFIG = (
+    "invenio_records_resources.services.RecordServiceConfig{InvenioRecordServiceConfig}"
+)
+RDM_SERVICE_CONFIG = "invenio_rdm_records.services.config.RDMRecordServiceConfig"
+
+DRAFT_SERVICE_CONFIG = "invenio_drafts_resources.services.RecordServiceConfig{InvenioRecordDraftsServiceConfig}"
+
+
 class RDMServiceComponent(DataTypeComponent):
     eligible_datatypes = [ModelDataType]
     depends_on = [ServiceModelComponent, ParentRecordComponent]
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
 
-        if not datatype.profile in ["record", "draft"]:
+        if datatype.profile not in ["record", "draft"]:
             return
         components_to_remove = [
-            '{{oarepo_runtime.services.files.FilesComponent}}',
-            '{{invenio_drafts_resources.services.records.components.DraftFilesComponent}}',
-            '{{oarepo_runtime.services.components.OwnersComponent}}'
+            "{{oarepo_runtime.services.files.FilesComponent}}",
+            "{{invenio_drafts_resources.services.records.components.DraftFilesComponent}}",
+            "{{oarepo_runtime.services.components.OwnersComponent}}",
         ]
         datatype.service_config["components"] = [
-            component for component in datatype.service_config["components"]
+            component
+            for component in datatype.service_config["components"]
             if component not in components_to_remove
         ]
-        if "base-classes" in datatype.definition["service"] and "invenio_records_resources.services.RecordService{InvenioRecordService}" in \
-                datatype.definition["service"]["base-classes"]:
-            datatype.definition["service"]["base-classes"].remove(
-                "invenio_records_resources.services.RecordService{InvenioRecordService}")
-            datatype.definition["service"]["base-classes"].append(
-                "invenio_rdm_records.services.services.RDMRecordService")
+        service_base_classes = datatype.definition["service"].setdefault(
+            "base-classes", []
+        )
+        if service_base_classes:
+            if PLAIN_RECORD_SERVICE in service_base_classes:
+                idx = service_base_classes.index(PLAIN_RECORD_SERVICE)
+                service_base_classes[idx] = RDM_RECORD_SERVICE
+            if DRAFT_RECORD_SERVICE in service_base_classes:
+                idx = service_base_classes.index(DRAFT_RECORD_SERVICE)
+                service_base_classes[idx] = RDM_RECORD_SERVICE
         else:
-            datatype.definition["service"]["base-classes"] = [
-                "invenio_rdm_records.services.services.RDMRecordService"]
+            service_base_classes.append(RDM_RECORD_SERVICE)
 
-        if "base-classes" in datatype.definition["service-config"] and "invenio_records_resources.services.RecordServiceConfig{InvenioRecordServiceConfig}" in \
-                datatype.definition["service-config"]["base-classes"]:
-            datatype.definition["service-config"]["base-classes"].remove(
-                "invenio_records_resources.services.RecordServiceConfig{InvenioRecordServiceConfig}")
-            datatype.definition["service-config"]["base-classes"].append(
-                "invenio_rdm_records.services.config.RDMRecordServiceConfig")
+        config_base_classes = datatype.definition["service-config"].setdefault(
+            "base-classes", []
+        )
+        if config_base_classes:
+            if PLAIN_SERVICE_CONFIG in config_base_classes:
+                idx = config_base_classes.index(PLAIN_SERVICE_CONFIG)
+                config_base_classes[idx] = RDM_SERVICE_CONFIG
+            if DRAFT_SERVICE_CONFIG in config_base_classes:
+                idx = config_base_classes.index(DRAFT_SERVICE_CONFIG)
+                config_base_classes[idx] = RDM_SERVICE_CONFIG
         else:
-            datatype.definition["service-config"]["base-classes"] = ["oarepo_runtime.services.config.service.PermissionsPresetsConfigMixin",
-                                                                 "invenio_rdm_records.services.config.RDMRecordServiceConfig"]
+            config_base_classes.append(RDM_SERVICE_CONFIG)
