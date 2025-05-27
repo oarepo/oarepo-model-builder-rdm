6c87a2606d4d90d7a48b6376c6066c1d96256c4a
Ronald Krist <ronald.krist@cesnet.cz>
2025-05-27 10:31:55 +0200

    base rdm resources from oarepo runtime

--- oarepo_model_builder_rdm/datatypes/components/resource.py
+++ oarepo_model_builder_rdm/datatypes/components/resource.py
@@ -2,6 +2,23 @@ from oarepo_model_builder.datatypes import DataTypeComponent
 from oarepo_model_builder.datatypes.components.model import ResourceModelComponent
 from oarepo_model_builder.datatypes.model import ModelDataType
 
+from oarepo_model_builder_rdm.datatypes.components.utils import replace_base_class
+
+PLAIN_RECORD_RESOURCE = (
+    "invenio_records_resources.resources.RecordResource"
+)
+DRAFT_RECORD_RESOURCE = (
+    "invenio_drafts_resources.resources.RecordResource"
+)
+RDM_RECORD_RESOURCE = "oarepo_runtime.resources.resource.BaseRecordResource"
+
+PLAIN_RESOURCE_CONFIG = (
+    "invenio_records_resources.resources.RecordResourceConfig"
+)
+DRAFT_RESOURCE_CONFIG = "invenio_drafts_resources.resources.RecordResourceConfig"
+RDM_RESOURCE_CONFIG = "oarepo_runtime.resources.config.BaseRecordResourceConfig"
+
+
 class RDMResourceComponent(DataTypeComponent):
     eligible_datatypes = [ModelDataType]
     depends_on = [ResourceModelComponent]
@@ -10,4 +27,27 @@ class RDMResourceComponent(DataTypeComponent):
 
         if datatype.profile not in ["record", "draft"]:
             return
-        datatype.definition["resource"]["base-classes"].insert(0, "oarepo_runtime.resources.resource.SearchAllResourceMixin")
+
+        replace_base_class(
+            datatype.definition["resource"],
+            PLAIN_RECORD_RESOURCE,
+            RDM_RECORD_RESOURCE,
+        )
+
+        replace_base_class(
+            datatype.definition["resource"],
+            DRAFT_RECORD_RESOURCE,
+            RDM_RECORD_RESOURCE,
+        )
+
+        replace_base_class(
+            datatype.definition["resource-config"],
+            PLAIN_RESOURCE_CONFIG,
+            RDM_RESOURCE_CONFIG,
+        )
+
+        replace_base_class(
+            datatype.definition["resource-config"],
+            DRAFT_RESOURCE_CONFIG,
+            RDM_RESOURCE_CONFIG,
+        )
